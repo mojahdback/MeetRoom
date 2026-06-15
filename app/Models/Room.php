0d0683a3999a5200974ecef7e556a80c0e8538b0
mohamed <mojahdontheway@gmail.com>
2026-06-15 09:00:46 +0100

    add model of room

---
+++ app/Models/Room.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+class Room extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'capacity',
+        'floor',
+        'is_active',
+        'building_id'
+    ];
+
+    public function building(){
+        return $this->belongsTo(Building::class);
+    }
+
+    public function equipements(){
+        return $this->belongsToMany(
+            Equipement::class,
+            'equipement_room',
+            'room_id',
+            'equipement_id'
+        );
+    }
+
+    
+}
