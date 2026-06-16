8f1a1950aa760f6d8a99550aee929cdb2feda72d
mohamed <mojahdontheway@gmail.com>
2026-06-16 17:05:44 +0100

    add realations bettween reservation and user and room in reservation model

---
+++ app/Models/Reservation.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+     use HasFactory;
+        // Reservation : start_datetime, end_datetime, status (pending | confirmed | cancelled), notes
+
+    protected $fillable = [
+        'start_datetime',
+        'end_datetime',
+        'status',
+        'notes',
+        'room_id',
+        'user_id'
+    ];
+
+
+    public function user(){
+        return $this->belongsTo(User::class);
+
+    }
+
+    public function room(){
+        return $this->belongsTo(Room::class);
+
+    }
+
+
+  
+}
