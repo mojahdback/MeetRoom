a53abf0c27ee66e249821845e2d3446346a14303
mohamed <mojahdontheway@gmail.com>
2026-06-16 17:03:21 +0100

    add table the reservations

---
+++ database/migrations/2026_06_15_083040_create_reservation_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Room;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->date('start_datetime');
+            $table->date('end_datetime');
+            $table->enum('status',['pending' , 'confirmed' , 'cancelled'])->default('pending');
+            $table->text('notes');
+            $table->foreignIdFor(Room::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
