7fb218b7dea90e430a3cae91aa9b78da03ce54ce
mohamed <mojahdontheway@gmail.com>
2026-06-16 09:31:05 +0100

    add table pivot for realation bettween room and equipement

---
+++ database/migrations/2026_06_12_153426_equipement_room.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Equipement;
+use App\Models\Room;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         Schema::create('equipement_room', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Room::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Equipement::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('equipement_room');
+    }
+};
