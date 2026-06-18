527013b6918914591cba5062d020388b607a6f38
mohamed <mojahdontheway@gmail.com>
2026-06-18 14:58:08 +0100

    feat: verfy data of reservation for update and store

---
+++ app/Http/Requests/Reservation/StoreReservationRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Reservation;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreReservationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'start_datetime' => ['required', 'date'],
+            'end_datetime' => ['required', 'date', 'after:start_datetime'],
+            // 'status' => ['required', 'in:pending,confirmed,cancelled'],
+            'notes' => ['nullable', 'string', 'max:300'],
+            // 'room_id' => ['required','integer', 'exists:rooms,id'],
+            
+        ];
+    }
+}
---
+++ app/Http/Requests/Reservation/UpdateReservationRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Reservation;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateReservationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+             'start_datetime' => ['required', 'date'],
+            'end_datetime' => ['required', 'date', 'after:start_datetime'],
+            'status' => ['required', 'in:pending,confirmed,cancelled'],
+            'notes' => ['nullable', 'string', 'max:1000'],
+            'room_id' => ['required', 'exists:rooms,id'],
+        ];
+    }
+}
